d5684c3638892035c0d5157e129031ac6531b8ea
Pavanesh <pavanesh2021@gmail.com>
2021-07-19 19:07:58 +0530

    Implemented code for React + Spring boot
    Implemented code for React + Spring boot

---
+++ src/main/java/com/capgemini/reactdashboard/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.capgemini.reactdashboard.config;
---
+++ src/main/java/com/capgemini/reactdashboard/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.capgemini.reactdashboard.service.dto;
